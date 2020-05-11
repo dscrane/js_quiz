cbb85fc001fa171f2cdced643624a6f40f8c4c9a
dscrane <daegancrane@gmail.com>
2020-05-11 18:09:07 -0400

    added a better answer set up and modal result desplay

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
-const submitButton = document.querySelector('input[type="submit"]');
+const submitButton = document.querySelector(".quiz__cta");
+const response = document.getElementsByClassName("quiz__answers");
+const modal = document.querySelector(".results__modal");
+const modalScore = document.querySelector(".results__display");
+const modalCloseButton = document.querySelector(".results__modal-close");
 
 submitButton.addEventListener("click", formSubmit);
+modalCloseButton.addEventListener("click", closeModal);
 
 function formSubmit(e) {
   e.preventDefault();
-  console.log(e);
-  const valOne = this.querySelector('input[type="radio"]').value;
 
-  console.log(valOne);
+  const quizScore = calcScore(response);
+  modal.style.display = "block";
+  modalScore.innerHTML = `${quizScore}`;
+}
+
+function calcScore(response) {
+  let score = 0;
+  for (let res of response) {
+    if (!res.value) return;
+    score = score + parseInt(res.value);
+  }
+  // alert(score);
+  return score;
+}
+
+function closeModal() {
+  modal.style.display = "none";
 }
